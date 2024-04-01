cf63d4782e8dbbe4d37ebfe08d6ab9ea9858a8cf
sbx_user1051 <sbx_user1051@169.254.66.45>
2024-04-01 14:02:23 +0000

    Mon Apr 01 14:02:23 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,4 +3,22 @@
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
+    price = {'A': 50, 'B': 30, 'C': 20, 'D': 15}
+    offers = {'A': (3, 130), 'B': (2, 45)}
+
+    total = 0
+
+    sku_count = {sku: skus.count(sku) for sku in set(skus)}
+    for sku, count in sku_count.items():
+        if sku not in price:
+            return -1
+
+        price_per_item = price[sku]
+        if sku in offers:
+            offer_quantity, offer_price = offers[sku]
+            offer_count = count // offer_quantity
+            remaining_count = count % offer_quantity
+            total += offer_count * offer_price + remaining_count * price_per_item
+        else:
+            total += count * price_per_item
     raise NotImplementedError()
